ea97ca2af1671639bccff62c3dfa7400cc15b5cc
sam mwaura <samhanremson@gmail.com>
2024-10-08 14:26:05 +0300

    arrayfind

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(record) {
+    
+    let winningYear = record.find(game => game.result === "W");
+  
+    return winningYear ? winningYear.year : undefined;
+  }
+  const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+  ];
+  
+  console.log(superbowlWin(record)); 
+  
